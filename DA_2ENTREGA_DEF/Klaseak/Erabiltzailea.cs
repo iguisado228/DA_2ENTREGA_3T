c35a5557d9e82463c6a6e89b28d072941fdbd113
Aratz006 <ara_hue_flo@goierrieskola.org>
2025-10-27 13:54:58 +0100

    rename klaseak karpeta

--- DA_2ENTREGA_DEF/Klaseak/Erabiltzailea.cs
+++ DA_2ENTREGA_DEF/Klaseak/Erabiltzailea.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DA_2ENTREGA_DEF.KlaseakDir
+namespace DA_2ENTREGA_DEF.Klaseak
 {
     public class Erabiltzailea
     {
